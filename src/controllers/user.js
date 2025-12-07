cd9a4b47d7279f2d39378dbbf502fa9a6d0ff3e6
Prashant Singh <pramar97singh@gmail.com>
2025-12-07 15:07:09 +0530

    added end point for refresh token and user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,10 +5,15 @@ const bcrypt = require("bcrypt");
 const generateTokens = (user) => {
   // token
   const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "3d",
+    expiresIn: "15min",
   });
-
-  return accessToken;
+  // refresh token
+  const refreshToken = jwt.sign(
+    { id: user._id },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: "7d" }
+  );
+  return { accessToken, refreshToken };
 };
 exports.registerUser = async (req, res) => {
   try {
@@ -35,7 +40,6 @@ exports.registerUser = async (req, res) => {
 };
 // login through password
 exports.login = async (req, res) => {
-  console.log(req.body);
   try {
     const { phone, password } = req.body;
     const user = await User.findOne({ phone });
@@ -44,211 +48,58 @@ exports.login = async (req, res) => {
     }
     const match = await bcrypt.compare(password, user.password);
     if (!match) return res.status(400).json({ message: "Incorrect password" });
-
-    const accessToken = generateTokens(user);
+    const { accessToken, refreshToken } = generateTokens(user);
+    user.refreshToken = refreshToken; // store in DB
+    await user.save();
     res.status(200).json({
       success: true,
       message: "Success",
       data: {
-        user: user,
-        token: accessToken,
+        user,
+        accessToken,
       },
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.refresh = async (req, res) => {
+  const { refreshToken } = req.body;
+  if (!refreshToken)
+    return res.status(401).json({ message: "Refresh token required" });
+  const user = await User.findOne({ refreshToken });
+  if (!user) return res.status(401).json({ message: "Invalid refresh token " });
+  try {
+    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+    const newAccessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "15m",
+    });
+
+    res.status(200).json({ accessToken: newAccessToken });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+// logout
+exports.logout = async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
+    await User.findOneAndUpdate({ refreshToken }, { refreshToken: null });
+    res.json({ message: "Logged out successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-// exports.refresh = async (req, res) => {
-//   const { refreshToken } = req.body;
-//   if (!refreshToken)
-//     return res.status(401).json({ message: "Refresh token required" });
-//   const user = await User.findOne({ refreshToken });
-//   if (!user) return res.status(401).json({ message: "Invalid refresh token " });
-//   try {
-//     jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
-//     const newAccessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-//       expiresIn: "15m",
-//     });
-
-//     res.status(200).json({ accessToken: newAccessToken });
-//   } catch (error) {
-//     res.status(500).json({ error: error });
-//   }
-// };
-// logout
-// exports.logout = async (req, res) => {
-//   try {
-//     const { refreshToken } = req.body;
-//     await User.findOneAndUpdate({ refreshToken }, { refreshToken: null });
-//     res.json({ message: "Logged out successfully" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-// get Profile
-// exports.getUserData = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.userId);
-//     return res.status(200).json({ success: true, user });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// exports.sendOtp = async (req, res) => {
-//   try {
-//     const { phone } = req.body;
-//     const otp = Math.floor(1000 + Math.random() * 9000).toString();
-//     const expiry = Date.now() + 5 * 60 * 1000;
-//     let user = await User.findOne({ phone });
-//     //  for mobile otp
-//     if (!user) {
-//       return res.status(400).json({ message: "User already doesn't exists" });
-//     } else {
-//       user.otp = otp;
-//       user.otpExpiry = expiry;
-//       await user.save();
-//     }
-//     res.json({
-//       success: true,
-//       message: "OTP sent",
-//       otp, // remove in production
-//     });
-//   } catch (error) {
-//     res.status(500).json({ success: false, error: error.message });
-//   }
-// };
-
-// exports.verifyOtp = async (req, res) => {
-//   try {
-//     const { phone, otp } = req.body;
-//     const user = await User.findOne({ phone });
-//     if (!user)
-//       return res
-//         .status(404)
-//         .json({ success: false, message: "User not found" });
-//     if (user.otp !== otp || Date.now() > user.otpExpiry)
-//       return res
-//         .status(400)
-//         .json({ success: false, message: "Invalid or expired OTP" });
-//     const { accessToken, refreshToken } = generateTokens(user);
-//     // success full
-//     user.otp = null;
-//     user.otpExpiry = null;
-//     user.refreshToken = refreshToken;
-//     await user.save();
-
-//     res.status(200).json({
-//       success: true,
-//       message: "OTP verified",
-//       data: {
-//         user: user,
-//         token: accessToken,
-//       },
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// const generateTokens = (user) => {
-//   // token
-//   const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-//     expiresIn: "15min",
-//   });
-//   // refresh token
-//   const refreshToken = jwt.sign(
-//     { id: user._id },
-//     process.env.JWT_REFRESH_SECRET,
-//     { expiresIn: "7d" }
-//   );
-//   return { accessToken, refreshToken };
-// };
-// exports.registerUser = async (req, res) => {
-//   try {
-//     const { name, phone, password,email } = req.body;
-//     const exist = await User.findOne({ phone });
-//     if (exist) {
-//       return res.status(400).json({ message: "User already exists" });
-//     }
-//     const hashPassword = await bcrypt.hash(password, 10);
-//     const user = await User.create({
-//       name,
-//       phone,
-//       email,
-//       password: hashPassword,
-//     });
-//     res.status(201).json({
-//       success: true,
-//       message: "Registration successful",
-//       data: user,
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// // login through password
-// exports.login = async (req, res) => {
-//   try {
-//     const { phone, password } = req.body;
-//     const user = await User.findOne({ phone });
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-//     const match = await bcrypt.compare(password, user.password);
-//     if (!match) return res.status(400).json({ message: "Incorrect password" });
-
-//     const { accessToken, refreshToken } = generateTokens(user);
-//     user.refreshToken = refreshToken; // store in DB
-//     await user.save();
-
-//     res.status(200).json({
-//       success: true,
-//       message: "Success",
-//       data: {
-//         user: user,
-//         token: accessToken,
-//       },
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-// exports.refresh = async (req, res) => {
-//   const { refreshToken } = req.body;
-//   if (!refreshToken)
-//     return res.status(401).json({ message: "Refresh token required" });
-//   const user = await User.findOne({ refreshToken });
-//   if (!user) return res.status(401).json({ message: "Invalid refresh token " });
-//   try {
-//     jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
-//     const newAccessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-//       expiresIn: "15m",
-//     });
-
-//     res.status(200).json({ accessToken: newAccessToken });
-//   } catch (error) {
-//     res.status(500).json({ error: error });
-//   }
-// };
-// // logout
-// exports.logout = async (req, res) => {
-//   try {
-//     const { refreshToken } = req.body;
-//     await User.findOneAndUpdate({ refreshToken }, { refreshToken: null });
-//     res.json({ message: "Logged out successfully" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-// // get Profile
-// exports.getUserData = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.userId);
-//     return res.status(200).json({ success: true, user });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
+// getUser
+exports.getUserData = async (req, res) => {
+  const id = req.body._id;
+  try {
+    const user = await User.findById(id);
+    return res.status(200).json({ success: true, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const { registerUser, login, getUserData } = require("../controllers/user.js");
+const {
+  registerUser,
+  login,
+  getUserData,
+  refresh,
+  logout,
+} = require("../controllers/user.js");
 const { auth } = require("../middleware/auth.js");
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", login);
-// get
+router.post("/auth/register", registerUser);
+router.post("/auth/refresh", refresh);
+router.post("/auth/login", login);
+router.post("/auth/logout", logout);
+router.get("/user", getUserData);
 // router.get("/profile", auth, getUserData);
 
 module.exports = router;
